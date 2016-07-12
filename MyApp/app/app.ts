805f6e2e4b0385587009d4a98166751d5d85b3b6
FazioNico <contact@nicolasfazio.ch>
2016-07-12 09:22:56 +0200

    test navigator.onLine + vertical-align content in focus sugar slide

--- MyApp/app/app.ts
+++ MyApp/app/app.ts
@@ -24,6 +24,9 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       //StatusBar.styleDefault();
+      if(navigator.onLine == false){
+        console.log('disconnected')
+      }
       StatusBar.show();
       StatusBar.overlaysWebView(false);
       StatusBar.styleLightContent();
