f26303d9312aaf6f369eb07f19e544a8708373e7
FazioNico <contact@nicolasfazio.ch>
2016-07-15 15:21:26 +0200

    cleaning routes.ts file

--- MyApp/app/providers/routes/routes.ts
+++ MyApp/app/providers/routes/routes.ts
@@ -1,40 +1,40 @@
-import { Injectable} from '@angular/core';
+import { Injectable}         from '@angular/core';
 
-import {HomePage} from '../../pages/home/home';
-import {ScanPage} from '../../pages/scan/scan';
-import {SearchPage} from '../../pages/search/search';
-import {ProductPage} from '../../pages/product/product';
-
-import { AddPage } from '../../pages/add/add';
+import { HomePage }           from '../../pages/home/home';
+import { ScanPage }           from '../../pages/scan/scan';
+import { SearchPage }         from '../../pages/search/search';
+import { ProductPage }        from '../../pages/product/product';
+import { AddPage }            from '../../pages/add/add';
 
 @Injectable()
 export class Routes {
-  routes:Object = {};
 
-  HOME:string="home";
-  SCAN:string="scan";
-  SEARCH: string="search";
-  PRODUCT: string="product";
-  PRODUCT_DETAIL: string="product-detail";
-  ADD: string= "add";
+  routes:Object               = {}
+  HOME: string                = "home"
+  SCAN: string                = "scan"
+  SEARCH: string              = "search"
+  PRODUCT: string             = "product"
+  ADD: string                 = "add"
 
   constructor(){
-    this.routes[this.HOME]=HomePage;
-    this.routes[this.SCAN]=ScanPage;
-    this.routes[this.SEARCH]=SearchPage;
-    this.routes[this.PRODUCT]=ProductPage;
-    this.routes[this.ADD]=AddPage;
+
+    this.routes[this.HOME]    = HomePage
+    this.routes[this.SCAN]    = ScanPage
+    this.routes[this.SEARCH]  = SearchPage
+    this.routes[this.PRODUCT] = ProductPage
+    this.routes[this.ADD]     = AddPage
+
   }
 
   getPage(id){
-    const route = this.routes[id];
-    return route;
+    const   route = this.routes[id]
+    return  route
   }
 
   getRootPage(){
-    //let root = (this.auth.authenticated()) ? this.getPage(this.TABS) : this.getPage(this.HOME)
-    let root = this.getPage(this.HOME)
-    return root;
+    //let   root = (this.auth.authenticated()) ? this.getPage(this.TABS) : this.getPage(this.HOME)
+    let     root = this.getPage(this.HOME)
+    return  root
   }
 
 }
