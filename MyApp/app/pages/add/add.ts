cf5322e5f589525d528b5e5ae465c87c87293a49
FazioNico <contact@nicolasfazio.ch>
2016-07-24 22:11:45 +0200

    clean code befor start & cut api-service call. replace by console.log('TEST')

--- MyApp/app/pages/add/add.ts
+++ MyApp/app/pages/add/add.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import {Camera} from 'ionic-native';
+/**
+* @Author: Nicolas Fazio <webmaster-fazio>
+* @Date:   12-07-2016
+* @Email:  contact@nicolasfazio.ch
+* @Last modified by:   webmaster-fazio
+* @Last modified time: 24-07-2016
+*/
 
-import { ApiService } from '../../providers/api-service/api-service';
-import { HeaderContent } from '../../components/header-content/header-content';
+import { Component }       from '@angular/core';
+import {
+  FORM_DIRECTIVES,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ControlGroup
+}                           from '@angular/common';
+import { NavController }    from 'ionic-angular';
+import {Camera}             from 'ionic-native';
 
-import { FORM_DIRECTIVES, FormBuilder, Validators, AbstractControl, ControlGroup } from '@angular/common';
+import { ApiService }       from '../../providers/api-service/api-service';
+import { HeaderContent }    from '../../components/header-content/header-content';
 
 /*
   Generated class for the AddPage page.
@@ -13,17 +26,23 @@ import { FORM_DIRECTIVES, FormBuilder, Validators, AbstractControl, ControlGroup
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+
 @Component({
   templateUrl: 'build/pages/add/add.html',
-  directives: [[HeaderContent],[FORM_DIRECTIVES]],
+  directives: [
+    HeaderContent,
+    FORM_DIRECTIVES
+  ],
   providers: [ApiService]
 })
 export class AddPage {
 
-  base64Image: any = null;
-  product:any = null;
   myForm: ControlGroup;
   barcode:string;
+  base64Image: any     = null;
+  product:any          = null;
+  productAdded:boolean = false;
+
   /*
   myData: any;
   name: string;
@@ -32,27 +51,28 @@ export class AddPage {
   ingredients: string;
   */
 
-  productAdded:boolean = false;
-
-  constructor(private nav: NavController,public apiService: ApiService, private fb: FormBuilder) {
-
+  constructor(
+    private nav       : NavController,
+    public apiService : ApiService,
+    private fb        : FormBuilder
+  ) {
 
     //this.myData = null;
     this.myForm = fb.group({
-      barcode: ["", Validators.required],
-      name: ["", Validators.required],
+      barcode:      ["", Validators.required],
+      name:         ["", Validators.required],
       denomination: ["", Validators.required],
-      quantity: ["", Validators.required],
-      ingredients: ["", Validators.required],
+      quantity:     ["", Validators.required],
+      ingredients:  ["", Validators.required],
       nutriments: fb.group({
-        energy: ["", Validators.required],
-        proteins: ["", Validators.required],
-        fat: ["", Validators.required],
-        fat_sat: ["", Validators.required],
-        sugar: ["", Validators.required],
-        sugar_sat: ["", Validators.required],
-        fiber: ["", Validators.required],
-        salt: ["", Validators.required],
+          energy:     ["", Validators.required],
+          proteins:   ["", Validators.required],
+          fat:        ["", Validators.required],
+          fat_sat:    ["", Validators.required],
+          sugar:      ["", Validators.required],
+          sugar_sat:  ["", Validators.required],
+          fiber:      ["", Validators.required],
+          salt:       ["", Validators.required],
       })
     })
   }
@@ -62,7 +82,7 @@ export class AddPage {
     console.log('pic starting')
     Camera.getPicture({
       destinationType: Camera.DestinationType.DATA_URL,
-      targetWidth: 400,
+      targetWidth:  400,
       targetHeight: 400
     })
     .then(
@@ -79,7 +99,7 @@ export class AddPage {
     console.log(this.myForm)
     //this.myData = formData;
 
-    if(this.myForm.status == 'INVALID'){
+    if(this.myForm.status == 'VALID'){
       console.log('Form valid -> submitted ');
       this.product                  = this.myForm.value
 
@@ -98,32 +118,32 @@ export class AddPage {
       dataReady.nutriment_proteins  = this.product.nutriments.proteins
       dataReady.nutriment_salt      = this.product.nutriments.salt
       dataReady.ingredients_text    = this.product.ingredients
-
       dataReady.lang                = 'fr';
 
-      //console.log(this.SerializeParams(dataReady))
-      let resultPost = this.apiService.save(this.SerializeParams(dataReady));
+      //let resultPost = this.apiService.save(this.SerializeParams(dataReady));
+      let resultPost = 'TEST: Post Form OK'
       console.log(resultPost)
       this.productAdded = true;
     }
   }
+
   onClickToggle(e){
-    //console.log(e)
     let el = e.target.closest(".acc-item > h3")
     el.nextElementSibling.classList.toggle("open");
     el.children[0].classList.toggle("rotate")
   }
 
-  SerializeParams<T>(Datas: T): string|number {
-     let keys:any = Object.keys(Datas);
+
+  SerializeParams<T>(Data: T): string|number {
+     let keys:any = Object.keys(Data);
      let stringParams: string = "?";
      for (let i in keys) {
          let name = keys[i];
-         if(Datas[name].length >= 1){
+         if(Data[name].length >= 1){
            if (i == '0')
-               stringParams += name + "=" + Datas[name];
+               stringParams += name + "=" + Data[name];
            else
-               stringParams += "&" + name + "=" + Datas[name]
+               stringParams += "&" + name + "=" + Data[name]
          }
      }
      return stringParams;
