cf8188a7febf8afeb7b910b36e945ae55774ca7f
FazioNico <contact@nicolasfazio.ch>
2016-07-25 07:03:29 +0200

    adding page login whitout auth

---
+++ MyApp/app/pages/login/login.ts
@@ -0,0 +1,63 @@
+/**
+* @Author: Nicolas Fazio <webmaster-fazio>
+* @Date:   25-07-2016
+* @Email:  contact@nicolasfazio.ch
+* @Last modified by:   webmaster-fazio
+* @Last modified time: 25-07-2016
+*/
+
+import { Component } from '@angular/core';
+import { NavController, Modal } from 'ionic-angular';
+
+import {Routes} from '../../providers/routes/routes';
+//import {Auth} from '../../providers/auth/auth';
+
+/*
+  Generated class for the LoginPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/login/login.html',
+})
+export class LoginPage {
+
+  email:string;
+  password:string;
+  error:string;
+
+  /** Not normally mandatory but create bugs if ommited. **/
+  static get parameters() {
+        return [[NavController], [Routes]];
+  }
+  constructor(
+    private nav: NavController,
+    private routes:Routes
+  ) {
+  }
+
+  login(){
+    /*
+    this.auth.login({email:this.email,password:this.password })
+      .then((success)=>{
+
+        this.goAdd();
+
+      },(error)=>{
+        this.error = error._body;
+      })
+    */
+  }
+
+  goAdd(){
+      this.nav.push(this.routes.getPage(this.routes.ADD))
+  }
+
+  openSignup(){
+    //let modal = Modal.create(this.routes.getPage(this.routes.SIGNUP));
+    //this.nav.present(modal);
+  }
+
+
+}
