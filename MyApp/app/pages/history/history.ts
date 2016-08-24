f7b58de2197672ef09849b95ff9f66a28100276d
FazioNico <contact@nicolasfazio.ch>
2016-08-24 12:01:39 +0200

    starting binging data from firebase to template

--- MyApp/app/pages/history/history.ts
+++ MyApp/app/pages/history/history.ts
@@ -3,6 +3,7 @@ import { NavController }    from 'ionic-angular';
 
 import { HeaderContent }    from '../../components/header-content/header-content';
 import { Routes }           from '../../providers/routes/routes';
+import { FirebaseService }  from '../../providers/firebase/firebase';
 /*
   Generated class for the HistoryPage page.
 
@@ -11,24 +12,65 @@ import { Routes }           from '../../providers/routes/routes';
 */
 @Component({
   templateUrl: 'build/pages/history/history.html',
-  directives: [HeaderContent]
+  directives: [HeaderContent],
+  providers: [
+    FirebaseService
+  ]
 })
 export class HistoryPage {
+
+  isAuth:boolean = false;
+  historySearch:any[] = [];
 
   /** Not normally mandatory but create bugs if ommited. **/
   static get parameters() {
         return [
           [NavController],
-          [Routes]
+          [Routes],
+          [FirebaseService]
         ];
   }
 
   constructor(
-    private nav: NavController,
-    private routes    : Routes
+    private nav       : NavController,
+    private routes    : Routes,
+    public authData   : FirebaseService
   ) {
+    //// Check if user Auth == true
+    let self = this;
+    let historySearch = [];
+    this.authData = authData;
+    this.authData.fireAuth.onAuthStateChanged((user) => {
+      if (user) {
+        this.isAuth = true
+        // save product id in history.save.list
+        let database = this.authData.database.ref('historySearch/' + user.uid);
+        database.on('value', function(snapshot) {
+          //console.log(snapshot.val());
+          if(snapshot.val() === null){
+            // no historySearch
+          }
+          else {
+            for (var key in snapshot.val()) {
+              if (snapshot.val().hasOwnProperty(key)) {
+                console.log(key, snapshot.val()[key])
+                historySearch.push(key)
+              }
+            }
+            //console.log('user historySearch database.ref() exist.')
+            console.log(snapshot.val())
+            self.searchDataProduct(historySearch)
+          }
+        });
+      } else {
+        this.isAuth = false
+      }
+    });
   }
 
+  searchDataProduct(arrayData){
+    this.historySearch = arrayData
+  }
 
   /** Events Methode **/
   onClickBack(){
